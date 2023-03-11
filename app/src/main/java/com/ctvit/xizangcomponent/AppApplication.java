f5c321e6cc1b8f3adeeb1c7bad0eb798f0c3a7ad
sungy8 <sungy8@lenovo.com>
2023-03-11 12:38:40 +0800

    1

--- app/src/main/java/com/ctvit/xizangcomponent/AppApplication.java
+++ app/src/main/java/com/ctvit/xizangcomponent/AppApplication.java
@@ -22,6 +22,12 @@ public class AppApplication extends BaseApplication {
         ModuleLifecycleConfig.getInstance().initModuleAhead(this);
 
 
+
+
+        
+
+        
+
         //....
         //初始化组件(靠后)
         ModuleLifecycleConfig.getInstance().initModuleLow(this);
